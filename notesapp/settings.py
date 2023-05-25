d8062a9e64dfcde86ddf32704d298016b30e5b6d
Maikyau Israel <maikyauisrael@gmail.com>
2023-05-25 13:04:04 +0100

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
