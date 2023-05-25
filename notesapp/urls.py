bda6593d0a69749ccc1c17544fb18c1d80b1ade0
Maikyau Israel <74977495+thebugged@users.noreply.github.com>
2023-05-26 00:35:01 +0100

    Add files via upload

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
 
     # Catch-all URL pattern for React app
-    path('', TemplateView.as_view(template_name = 'index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
