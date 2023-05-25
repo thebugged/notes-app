6c7831a6956c72b85dc5479e6d81a7f1eaced3b2
Maikyau Israel <maikyauisrael@gmail.com>
2023-05-25 14:07:34 +0100

    update

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -127,9 +127,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'frontend/build/static'
+# ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
